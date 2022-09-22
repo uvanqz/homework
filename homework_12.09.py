98ac562ae656165fd4699ad103a769ce76674b8a
uvanqz <113163658+uvanqz@users.noreply.github.com>
2022-09-22 06:40:48 +0000

    Create homework_12.09.py

---
+++ homework_12.09.py
@@ -0,0 +1,16 @@
+def percents(str):
+    big_str = 0
+    stroka = str.split(" ")
+    for podstroka in stroka:
+        upp = 0  
+        low = 0 
+        for element in podstroka:
+            if element.isupper(): 
+                upp +=1
+            if element.islower():
+                low +=1
+        if upp > low :
+            big_str +=1 
+    otvet = (big_str*100)/ len(stroka)
+    return otvet
+print(percents('NDp Lka nuR vtE'), '%')
