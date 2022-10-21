f902e627a86686591aad7d36951a9dda8eb25a11
uvanqz <113163658+uvanqz@users.noreply.github.com>
2022-10-21 19:07:20 +0000

    Update 1909.py

--- 1909.py
+++ 1909.py
@@ -1,16 +1,13 @@
-import calendar
-
 def proverka_dats(day, month, year):
     months_31 = [1, 3, 5, 7, 8, 10, 12]
     months_30 = [4, 6, 9, 11]
-    proverka_year=calendar.isleap(year)
     if 1 <= day <= 31 and month in months_31:
         return True
     if 1 <= day <= 30 and month in months_30:
         return True
-    if proverka_year == True and month == 2 and 1 <= day <= 29:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0 and month == 2 and 1 <= day <= 29:
         return True
-    if proverka_year == False and month == 2 and 1 <= day <= 28:
+    if year % 4 != 0 and month == 2 and 1 <= day <= 28:
         return True
     else:
         return False
