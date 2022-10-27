0c09aaae9a93e1d711c0c0e26d8032469a04a1c2
uvanqz <113163658+uvanqz@users.noreply.github.com>
2022-10-27 08:41:01 +0000

    Update 2809.py

--- 2809.py
+++ 2809.py
@@ -4,9 +4,9 @@ from datetime import datetime, timedelta
 #timedelta — этот атрибут покрывает интервалы и используется для определения прошлых или будущих событий
 def capitan(tekst, data):
     next_day = timedelta(days=1)
-    with open ("capitan.txt", 'w') as file:
+    with open("capitan.txt", 'w') as file:
         for element in tekst:
-            file.write(str(data) + ' : ' + element + '\n')  #str преобразует данные в строку
-            data = data + next_day
-        file.close()
+            file.write(str(data) + ' : ' + element + '\n') 
+            data += next_day
+
 capitan(['Вышли в открытое море','Сильный ураган','Погода улучшилась', 'Виднеется земля'], datetime (2022, 10, 12))
